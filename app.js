55292c6f01be67f8dd80459ee362ae9423628f36
o-k-a-y <btunney@ramapo.edu>
2020-02-16 22:18:43 -0500

    Made route to get users

--- app.js
+++ app.js
@@ -1,41 +1,70 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
+
+// TODO: Add other collections
+let users;
+
+const MongoClient = require("mongodb").MongoClient;
+const ObjectID = require("mongodb").ObjectID;
+const url = process.env.MONGO_URL || "mongodb://localhost:27017/quackchat";
+
+// Setup and connect to MongoDB
+const startup = async () => {
+    try {
+        const connection = await MongoClient.connect(url, {
+            useUnifiedTopology: true
+        });
+        const db = connection.db("quackchat");
+        users = await db.createCollection("users");
+    } catch (ex) {
+        console.error(ex);
+    }
+};
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+// Add MongoDB collections to middleware and add the MongoDB collection object to the request object
+app.use((req, res, next) => {
+    req.usersCollection = users;
+
+    next(); // allow us to continue to use other middlewares (go to the NEXT middleware in the chain)
+});
+
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+    next(createError(404));
 });
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get("env") === "development" ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render("error");
 });
 
+
+startup()
 module.exports = app;
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,22 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+const ObjectID = require("mongodb").ObjectID;
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get("/", async function(req, res, next) {
+    let allUsers;
+    console.log(req);
+
+    try {
+        allUsers = await req.usersCollection.find({}).toArray();
+        res.json(allUsers);
+        //console.log(allUser);
+    } catch (ex) {
+        console.log(ex);
+    }
 });
+
+/* POST new user to mongoDB */
+router.post("/", async function(req, res, next) {});
 
 module.exports = router;
