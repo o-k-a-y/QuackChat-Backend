edebaeb06ebe379cc03e34d8f28fd5cf711a1d45
o-k-a-y <btunney@ramapo.edu>
2020-02-22 23:30:37 -0500

    Added put to add user to mongoDB database

--- routes/users.js
+++ routes/users.js
@@ -22,4 +22,22 @@ router.post("/", function(req, res, next) {
     res.json({"test": "test"})
 });
 
+
+router.put("/", async function(req, res, next) {
+    // // Return nothing if data is invalid
+    // if (!validateData(req.body)) {
+    //     res.json({});
+    // }
+    
+    let user = formatData(req.body);
+
+    // Insert contact into DB
+    try {
+        let inserted = await req.usersCollection.insertOne(user);
+    } catch (ex) {
+        console.log(ex);
+    }
+})
+
+
 module.exports = router;
