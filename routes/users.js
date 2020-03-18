fe23064ff43ec7f8f7cb17a1a204637642014a4c
o-k-a-y <btunney@ramapo.edu>
2020-03-18 18:43:21 -0400

    Check if user exists

--- routes/users.js
+++ routes/users.js
@@ -32,7 +32,8 @@ router.put("/", async function(req, res, next) {
     
     let user = req.body;
 
-    // TODO Make new object and pull out only needed fields
+    // TODO verify user doesn't already exist
+
 
     let salt = await bcrypt.genSalt(10);
     let hash = await bcrypt.hash(user.password, salt);
@@ -45,26 +46,34 @@ router.put("/", async function(req, res, next) {
     
     // Insert contact into DB
     try {
+
+        // Check if user already exists
+
         let inserted = await req.usersCollection.insertOne(user);
         var newUser = inserted.ops;
         newUser = newUser[0];
     } catch (ex) {
+        // Can't connect to DB
         console.log(ex);
+        res.status(500);
+        return;
+
     }
 
-    res.json(newUser);
+    res.status(201);
+    return;
+
+    //res.json(newUser);
 })
 
 const sanitizeInput = (user) => {
     return {username: user.username, email: user.email, user: user.password};
 }
 
-
-// TODO
-const formatData = (data) => {
-    let JSONData;
-
+const doesUserExist = (username) => {
+    return await req.collections.users.findOne({ user: { $regex: new RegExp(username, "i") } });
 }
+
 
 
 module.exports = router;
