d2be2c7b892394dcc790b64ee99060026bd0cc75
o-k-a-y <btunney@ramapo.edu>
2020-03-18 23:26:08 -0400

    formatting

--- routes/users.js
+++ routes/users.js
@@ -18,20 +18,20 @@ router.get("/", async function(req, res, next) {
 });
 
 /* POST login */
-router.post('/login', async function(req, res, next) {
-	console.log(req.usersCollection);
-	// Get username and password fields
-	let username = req.body.username;
-	let password = req.body.password;
-	
-	console.log(username, password);
-	console.log(req.body);
+router.post("/login", async function(req, res, next) {
+    console.log(req.usersCollection);
+    // Get username and password fields
+    let username = req.body.username;
+    let password = req.body.password;
 
-	let user;
+    console.log(username, password);
+    console.log(req.body);
+
+    let user;
     // Find user in DB
     try {
-        user = await req.usersCollection.findOne({username: username});
-        console.log("user: " +  user);
+        user = await req.usersCollection.findOne({ username: username });
+        console.log("user: " + user);
 
         // No user exists with that username
         if (!user) {
@@ -39,11 +39,11 @@ router.post('/login', async function(req, res, next) {
             return;
         }
 
-		// Set session variables if correct password
-		console.log(user.password);
-		const match = await bcrypt.compare(password, user.password);
-		
-		if (match) {
+        // Set session variables if correct password
+        console.log(user.password);
+        const match = await bcrypt.compare(password, user.password);
+
+        if (match) {
             console.log("user exists");
             req.username = username;
             req.userId = user._id;
@@ -53,7 +53,6 @@ router.post('/login', async function(req, res, next) {
             res.status(401).send();
             return;
         }
-
     } catch (ex) {
         console.log(ex);
         res.status(500).send();
@@ -63,21 +62,19 @@ router.post('/login', async function(req, res, next) {
 
 /* POST new user to mongoDB */
 router.post("/", function(req, res, next) {
-    console.log(req.body)
-    res.json({"test": "test"})
+    console.log(req.body);
+    res.json({ test: "test" });
 });
-
 
 router.put("/", async function(req, res, next) {
     // // Return nothing if data is invalid
     // if (!validateData(req.body)) {
     //     res.json({});
     // }
-    
+
     let user = req.body;
 
     // TODO verify user doesn't already exist
-
 
     let salt = await bcrypt.genSalt(10);
     let hash = await bcrypt.hash(user.password, salt);
@@ -87,18 +84,21 @@ router.put("/", async function(req, res, next) {
 
     //console.log(user.username);
     //console.log(user.password);
-    
+
     // Insert contact into DB
     try {
-
         // Check if user already exists
-        let identicalUser = await req.usersCollection.findOne({ username: { $regex: new RegExp(user.username, "i") } });
+        let identicalUser = await req.usersCollection.findOne({
+            username: { $regex: new RegExp(user.username, "i") }
+        });
         console.log(identicalUser);
         console.log(user.username);
-		
-		if (identicalUser) {
-           res.status(409).json({message: "User already exists"}).send();
-           return;
+
+        if (identicalUser) {
+            res.status(409)
+                .json({ message: "User already exists" })
+                .send();
+            return;
         }
 
         await req.usersCollection.insertOne(user);
@@ -110,20 +110,21 @@ router.put("/", async function(req, res, next) {
         console.log(ex);
         res.status(500).send();
         return;
-
     }
 
     res.status(201).send();
     return;
 
     //res.json(newUser);
-})
+});
 
-const sanitizeInput = (user) => {
+const sanitizeInput = user => {
     // TODO: find method of checking each of these fields are not empty/NULL and password length is right length
-    return {username: user.username, email: user.email, password: user.password};
-}
-
-
+    return {
+        username: user.username,
+        email: user.email,
+        password: user.password
+    };
+};
 
 module.exports = router;
